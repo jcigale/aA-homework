1b1c7cbceb7fe9f50ae7d076c8e89c6dcca422fe
John Cigale <jcigale98@gmail.com>
2021-01-29 09:03:37 -0500

    W5D5

---
+++ W5D4/demo_project/db/migrate/20210128145340_person.rb
@@ -0,0 +1,10 @@
+class Person < ActiveRecord::Migration[5.1]
+  validates :name, :house_id, presence: true
+  def change
+    belongs_to( :house,
+    class_name: House,
+    primary_key: id,   
+    foreign_key: house_id 
+    )
+  end
+end
