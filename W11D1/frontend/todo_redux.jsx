2abc1933537a51ead74a94ccf0229a1860b6d703
John Cigale <jcigale98@gmail.com>
2021-03-08 09:02:34 -0500

    W11D1

---
+++ W11D1/frontend/todo_redux.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+function addLoggingToDispatch(store) {
+  const dispatchedStore = store.dispatch;
+  return ((action) => {
+      console.log(store.getState());
+      console.log(action);
+      dispatchedStore(action);
+      console.log(store.getState())
+    }
+  )
+
+} 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+
+  store.dispatch = addLoggingToDispatch(store);
+  
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
